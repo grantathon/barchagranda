5894d83968cd63a6bef398f2fb1a9585dd273a66
grantathon <grant.bartel@gmail.com>
2016-06-26 22:05:51 +0200

    Completed ANN classifier class, integrated class into selector

---
+++ ArtificialNeuralNetworkClassifier.py
@@ -0,0 +1,112 @@
+import tensorflow as tf
+import random
+from pprint import pprint
+
+
+class ArtificialNeuralNetworkClassifier(object):
+	def __init__(self, session, neurons, dropout_rates):
+		if((len(neurons) - 2) < 0):
+			raise ValueError('There must be at least an input and output layer.')
+
+		# Initialize data members
+		self.layer_neurons = neurons
+		self.num_input_neurons = neurons[0]
+		self.hidden_neurons = neurons[1:len(neurons)-1]
+		self.num_output_neurons = neurons[-1]
+		self.dropout_rates = dropout_rates
+		self.num_dropout_rates = len(dropout_rates)
+		self.num_hidden_layers = len(self.hidden_neurons)
+
+		if(self.num_hidden_layers != self.num_dropout_rates):
+			raise ValueError('The number of dropout rates and hidden layers must be equal.')
+
+		self.__create_ann(session)
+
+	def __weight_variable(self, shape):
+		initial = tf.truncated_normal(shape, stddev=0.1)
+		return tf.Variable(initial)
+
+	def __bias_variable(self, shape):
+		initial = tf.constant(0.1, shape=shape)
+		return tf.Variable(initial)
+
+	def train(self, session, features, labels, num_iterations, batch_size, verbose=False):
+		# Iteratively train the ANN
+		for i in range(num_iterations):
+			# Periodically evaluate accuracy
+			if(verbose and i % 100 == 0):
+				eval_drs = [1.0 for j in range(self.num_dropout_rates)]
+				train_log_loss = self.cross_entropy.eval(feed_dict={self.x: features, self.y_: labels, self.dr: eval_drs})
+				print("Step %d, training log loss %.5f" % (i, train_log_loss))
+
+			# Generate random indices and run training step
+			idx = random.sample(xrange(len(features)-1), batch_size)
+			self.train_step.run(feed_dict={self.x: features[idx], self.y_: labels[idx], self.dr: self.dropout_rates})
+
+	def predict(self, session, features):
+		return self.y.eval(feed_dict={self.x: features})
+
+	def log_loss(self, session, features, labels):
+		eval_drs = [1.0 for i in range(self.num_dropout_rates)]
+		return self.cross_entropy.eval(feed_dict={self.x: features, self.y_: labels, self.dr: eval_drs})
+
+	def matches(self, session, features, labels):
+		eval_drs = [1.0 for i in range(self.num_dropout_rates)]
+		return self.accuracy.eval(feed_dict={self.x: features, self.y_: labels, self.dr: eval_drs})
+
+	def __create_ann(self, session):
+		# Placeholders for training examples and labels
+		self.x = tf.placeholder(tf.float32, [None, self.num_input_neurons])
+		self.y_ = tf.placeholder(tf.float32, [None, self.num_output_neurons])
+
+		# Placeholder for dropout rates
+		self.dr = tf.placeholder(tf.float32, [self.num_dropout_rates])
+
+		# Construct the ANN layer by layer
+		W = []
+		b = []
+		h = []
+		h_drop = []
+		for i in range(self.num_hidden_layers):
+			# Determine proper dimensions for weight matrix and bias vector
+			if(i != 0):
+				input_count = self.layer_neurons[i-1]
+			else:
+				input_count = self.num_input_neurons
+			output_count = self.hidden_neurons[i]
+
+			# Create weight matrix and bias vector
+			W.append(self.__weight_variable([input_count, output_count]))
+			b.append(self.__bias_variable([output_count]))
+
+			# Create hidden layer
+			if(i != 0):
+				h.append(tf.nn.relu(tf.matmul(h_drop[i-1], W[i]) + b[i]))
+			else:
+				h.append(tf.nn.relu(tf.matmul(self.x, W[i]) + b[i]))
+
+			# Perform dropout on hidden layer
+			h_drop.append(tf.nn.dropout(h[i], self.dr[i]))
+			
+			# Check if hidden layer is connected to output layer
+			if(i == self.num_hidden_layers-1):
+				# Create weight matrix and bias vector
+				W.append(self.__weight_variable([output_count, self.num_output_neurons]))
+				b.append(self.__bias_variable([self.num_output_neurons]))
+
+				# Create output layer
+				self.y = tf.nn.softmax(tf.matmul(h_drop[i], W[i+1]) + b[i+1])
+
+		# Implement cross-entropy (i.e., log loss) as loss function
+		self.cross_entropy = tf.reduce_mean(-tf.reduce_sum(self.y_*tf.log(self.y), reduction_indices=[1]))
+
+		# Initialize optimizer
+		# train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+		self.train_step = tf.train.AdamOptimizer(1e-4).minimize(self.cross_entropy)
+
+		# Determine the accuracy of the model
+		self.correct_prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(self.y_,1))
+		self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+
+		# Initialize variables
+		session.run(tf.initialize_all_variables())
--- ann_model_selector.py
+++ ann_model_selector.py
@@ -3,19 +3,62 @@ import pandas as pd
 import numpy as np
 import json
 import random
+from ArtificialNeuralNetworkClassifier import ArtificialNeuralNetworkClassifier
 from sklearn.cross_validation import KFold
 from datetime import datetime
 from pprint import pprint
 from read_numerai import *
 
 
-def weight_variable(shape):
-	initial = tf.truncated_normal(shape, stddev=0.1)
-	return tf.Variable(initial)
+def create_parameter_sets(hidden_layer_spaces, dropout_rate_space):
+	parameter_sets = []
 
-def bias_variable(shape):
-	initial = tf.constant(0.1, shape=shape)
-	return tf.Variable(initial)
+	layer_neuron_scenarios = []
+	dropout_rate_scenarios = []
+	dropout_rates = np.linspace(dropout_rate_space[0], dropout_rate_space[1], dropout_rate_space[2])
+
+	# Go through all hidden layer spaces to construct ANN parameters
+	for hls in hidden_layer_spaces:
+		num_hidden_layers = len(hls)
+		num_dropout_rates = len(dropout_rates)
+
+		if(num_hidden_layers == 0):
+			parameter_dict = {}
+			parameter_dict['hidden_layers'] = None
+			parameter_dict['dropout_rates'] = None
+			parameter_sets.append(parameter_dict)
+		elif(num_hidden_layers == 1):
+			for i in range(num_dropout_rates):
+				for j in range(hls[num_hidden_layers-1][0], hls[num_hidden_layers-1][1]+1):
+					parameter_dict = {}
+					parameter_dict['hidden_layers'] = [j]
+					parameter_dict['dropout_rates'] = [round(dropout_rates[i], 4)]
+					parameter_sets.append(parameter_dict)
+		elif(num_hidden_layers == 2):
+			for i in range(num_dropout_rates):
+				for m in range(num_dropout_rates):
+					for j in range(hls[num_hidden_layers-1][0], hls[num_hidden_layers-1][1]+1):
+						for k in range(j, hls[num_hidden_layers-2][1]+1):
+							parameter_dict = {}
+							parameter_dict['hidden_layers'] = [k, j]
+							parameter_dict['dropout_rates'] = [round(dropout_rates[i], 4), round(dropout_rates[m], 4)]
+							parameter_sets.append(parameter_dict)
+		elif(num_hidden_layers == 3):
+			for i in range(num_dropout_rates):
+				for n in range(num_dropout_rates):
+					for p in range(num_dropout_rates):
+						for j in range(hls[num_hidden_layers-1][0], hls[num_hidden_layers-1][1]+1):
+							for k in range(j, hls[num_hidden_layers-2][1]+1):
+								for m in range(k, hls[num_hidden_layers-3][1]+1):
+									parameter_dict = {}
+									parameter_dict['hidden_layers'] = [m, k, j]
+									parameter_dict['dropout_rates'] = [round(dropout_rates[i], 4), round(dropout_rates[n], 4), \
+										round(dropout_rates[p], 4)]
+									parameter_sets.append(parameter_dict)
+		else:
+			raise NotImplementedError('Does not support %i hidden layers' % num_hidden_layers)
+
+	return parameter_sets
 
 
 if __name__ == "__main__":
@@ -28,69 +71,33 @@ if __name__ == "__main__":
 	with open(config_uri, mode='r') as f:
 		config_data = json.loads(f.read())
 
-	# Get model selector parameters
-	layer_count = int(config_data['layer_count'])
-	input_neuron_count = int(config_data['input_neuron_count'])
-	output_neuron_count = int(config_data['output_neuron_count'])
-	example_count = int(config_data['example_count'])
+	# Get model selector config parameters
+	num_input_neurons = int(config_data['num_input_neurons'])
+	num_output_neurons = int(config_data['num_output_neurons'])
+	hidden_layer_spaces = config_data['hidden_layer_spaces']
+	num_examples = int(config_data['num_examples'])
 	k_cross_folds = int(config_data['k_cross_folds'])
 	dropout_rate_space = config_data['dropout_rate_space']
-	iteration_count = int(config_data['iteration_count'])
+	num_iterations = int(config_data['num_iterations'])
 	batch_size = int(config_data['batch_size'])
 
-	# Get system parameters
+	# Get system config parameters
 	data_reader_uri = config_data['data_reader_uri']
 	data_uri = config_data['data_uri']
 	log_uri = config_data['log_uri']
 	results_uri = config_data['results_uri']
 
+	# Create the parameters needed to setup multiple scenarios of ANN archictures
+	parameter_sets = create_parameter_sets(hidden_layer_spaces, dropout_rate_space)
+
 	# Read training and testing data
 	print('Loading data...')
-	features, _, _, labels, _, _ = read_numerai(data_uri, example_count, 0, 1)
+	features, _, _, labels, _, _ = read_numerai(data_uri, num_examples, 0, 1)
 	print('Data loaded!')
 	print
 
 	# Determine the k-fold cross validation indicies for training and testing
-	k_folds_idx = KFold(example_count, n_folds=k_cross_folds)
-
-	# Determine layer-neuron and dropout rate scenarios
-	# TODO: Make this recursive
-	layer_neuron_scenarios = []
-	dropout_rate_scenarios = []
-	dropout_rates = np.linspace(dropout_rate_space[0], dropout_rate_space[1], dropout_rate_space[2])
-	for i in range(layer_count):
-		layer_neuron_scenarios.append([])
-		dropout_rate_scenarios.append([])
-		
-		if(i == 0):
-			for j in range(output_neuron_count, input_neuron_count+1):
-				layer_neuron_scenarios[i].append([j])
-			for j in range(len(dropout_rates)):
-				dropout_rate_scenarios[i].append([round(dropout_rates[j], 4)])
-		elif(i == 1):
-			for j in range(output_neuron_count, input_neuron_count+1):
-				for k in range(output_neuron_count, j+1):
-					layer_neuron_scenarios[i].append([j, k])
-			for j in range(len(dropout_rates)):
-				for k in range(len(dropout_rates)):
-					dropout_rate_scenarios[i].append([round(dropout_rates[j], 4), round(dropout_rates[k], 4)])
-		elif(i == 2):
-			for j in range(output_neuron_count, input_neuron_count+1):
-				for k in range(output_neuron_count, j+1):
-					for l in range(output_neuron_count, k+1):
-						layer_neuron_scenarios[i].append([j, k, l])
-			for j in range(len(dropout_rates)):
-				for k in range(len(dropout_rates)):
-					for l in range(len(dropout_rates)):
-						dropout_rate_scenarios[i].append([round(dropout_rates[j], 4), round(dropout_rates[k], 4), \
-							round(dropout_rates[l], 4)])
-		else:
-			raise NotImplementedError('Does not support %i hidden layers' % (i+1))
-
-	# Determine number of total scenarios to run
-	total_layer_neuron_scenarios = np.sum([len(layer_neuron_scenarios[i]) for i in range(layer_count)])
-	total_dropout_scenarios = np.sum([len(dropout_rate_scenarios[i]) for i in range(layer_count)])
-	total_scenarios = total_layer_neuron_scenarios * total_dropout_scenarios
+	k_folds_idx = KFold(num_examples, n_folds=k_cross_folds)
 
 	# Normalize features
 	for i in range(features.shape[1]):
@@ -103,127 +110,71 @@ if __name__ == "__main__":
 		'testing_accuracy': [],
 		'training_log_loss': [],
 		'training_accuracy': [],
-		'layer_count': [],
-		'neuron_counts': [],
+		'num_layers': [],
+		'num_neurons': [],
 		'dropout_rates': [],
 		'k_cross_folds': [],
-		'example_count': [],
-		'iteration_count': [],
+		'num_examples': [],
+		'num_iterations': [],
 		'batch_size': [],
 	}
 
 	# Start TensorFlow session
 	sess = tf.InteractiveSession()
 
-	# Evaluate all possible scenarios
-	# TODO: Try to extract some session placeholders, variables, and other things
+	# Evaluate each ANN parameter set
 	n = 1
-	for i in range(layer_count):
-		for dropout_rate_scenario in dropout_rate_scenarios[i]:
-			drs = [dropout_rate_scenario[j] for j in range(i+1)]
-			eval_drs = [1.0 for i in range(len(drs))]
+	for parameter_set in parameter_sets:
+		print('Running scenario %d/%d' % (n, len(parameter_sets)))
+		hidden_layers = parameter_set['hidden_layers']
+		dropout_rates = parameter_set['dropout_rates']
+		num_hidden_layers = len(hidden_layers)
+		num_dropout_rates = len(dropout_rates)
 
-			for layer_neuron_scenario in layer_neuron_scenarios[i]:
-				print('Running scenario %d/%d' % (n, total_scenarios))
-				n += 1
+		# Setup neuron-layer
+		neurons = [num_input_neurons, num_output_neurons]
+		if(parameter_set['hidden_layers']):
+			for i in range(num_hidden_layers):
+				neurons.insert(i+1, parameter_set['hidden_layers'][i])
+		else:
+			raise NotImplementedError('Logistic regression is not yet supported.')
 
-				layer_neurons = [layer_neuron_scenario[j] for j in range(i+1)]
+		# Create ANN
+		ann = ArtificialNeuralNetworkClassifier(sess, neurons, dropout_rates)
 
-				# Placeholders for training examples and labels
-				x = tf.placeholder(tf.float32, [None, input_neuron_count])
-				y_ = tf.placeholder(tf.float32, [None, output_neuron_count])
+		# Setup k-fold cross validation results
+		kf_results = {}
+		kf_results['training_log_loss'] = []
+		kf_results['training_accuracy'] = []
+		kf_results['testing_log_loss'] = []
+		kf_results['testing_accuracy'] = []
 
-				# Placeholder for dropout rates
-				dr = tf.placeholder(tf.float32, [len(drs)])
+		# Train and predict k times
+		for training_idx, testing_idx in k_folds_idx:
+			ann.train(sess, features[training_idx], labels[training_idx], num_iterations, batch_size, False)
 
-				# Construct the ANN layer by layer
-				W = []
-				b = []
-				h = []
-				h_drop = []
-				for j in range(i+1):
-					# Determine proper dimensions for weight matrix and bias vector
-					if(j != 0):
-						input_count = layer_neurons[j-1]
-					else:
-						input_count = input_neuron_count
-					output_count = layer_neurons[j]
+			# Compute the training accuracies
+			kf_results['training_log_loss'].append(ann.log_loss(sess, features[training_idx], labels[training_idx]))
+			kf_results['training_accuracy'].append(ann.matches(sess, features[training_idx], labels[training_idx]))
 
-					# Create weight matrix and bias vector
-					W.append(weight_variable([input_count, output_count]))
-					b.append(bias_variable([output_count]))
+			# Compute the testing accuracies
+			kf_results['testing_accuracy'].append(ann.log_loss(sess, features[testing_idx], labels[testing_idx]))
+			kf_results['testing_log_loss'].append(ann.matches(sess, features[testing_idx], labels[testing_idx]))
 
-					# Perform dropout on hidden layer
-					h_drop.append(tf.nn.dropout(h[j], dr[j]))
-					
-					# Create hidden layer
-					if(j != 0):
-						h.append(tf.nn.relu(tf.matmul(h_drop[j-1], W[j]) + b[j]))
-					else:
-						h.append(tf.nn.relu(tf.matmul(x, W[j]) + b[j]))
+		# Store results for later analysis
+		results['training_log_loss'].append(np.mean(kf_results['training_log_loss']))
+		results['training_accuracy'].append(np.mean(kf_results['training_accuracy']))
+		results['testing_log_loss'].append(np.mean(kf_results['testing_log_loss']))
+		results['testing_accuracy'].append(np.mean(kf_results['testing_accuracy']))
+		results['num_layers'].append(num_hidden_layers)
+		results['num_neurons'].append(hidden_layers)
+		results['dropout_rates'].append(dropout_rates)
+		results['k_cross_folds'].append(k_cross_folds)
+		results['num_examples'].append(num_examples)
+		results['num_iterations'].append(num_iterations)
+		results['batch_size'].append(batch_size)
 
-					# Check if hidden layer is connected to output layer
-					if(j == i):
-						# Create weight matrix and bias vector
-						W.append(weight_variable([output_count, output_neuron_count]))
-						b.append(bias_variable([output_neuron_count]))
-
-						# Create output layer
-						y = tf.nn.softmax(tf.matmul(h_drop[j], W[j+1]) + b[j+1])
-
-				# Implement cross-entropy (i.e., log loss) as loss function
-				cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y), reduction_indices=[1]))
-
-				# Initialize optimizer
-				# train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-				train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-
-				# Determine the accuracy of the model
-				correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-				accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-				# Initialize variables
-				sess.run(tf.initialize_all_variables())
-
-				# Setup k-fold cross validation results
-				kf_results = {}
-				kf_results['training_log_loss'] = []
-				kf_results['training_accuracy'] = []
-				kf_results['testing_log_loss'] = []
-				kf_results['testing_accuracy'] = []
-
-				# Train and test ANN k times
-				for training_idx, testing_idx in k_folds_idx:
-					# Iteratively train the ANN
-					for j in range(iteration_count):
-						# Generate a batch of random training indices and run training step
-						idx = random.sample(training_idx, batch_size)
-						train_step.run(feed_dict={x: features[idx], y_: labels[idx], dr: drs})
-
-					# Compute the training accuracies
-					kf_results['training_log_loss'].append(cross_entropy.eval(feed_dict={x: features[training_idx], \
-						y_: labels[training_idx], dr: eval_drs}))
-					kf_results['training_accuracy'].append(accuracy.eval(feed_dict={x: features[training_idx], \
-						y_: labels[training_idx], dr: eval_drs}))
-
-					# Compute the testing accuracies
-					kf_results['testing_accuracy'].append(accuracy.eval(feed_dict={x: features[testing_idx], \
-						y_: labels[testing_idx], dr: eval_drs}))
-					kf_results['testing_log_loss'].append(cross_entropy.eval(feed_dict={x: features[testing_idx], \
-						y_: labels[testing_idx], dr: eval_drs}))
-
-				# Store results for later analysis
-				results['training_log_loss'].append(np.mean(kf_results['training_log_loss']))
-				results['training_accuracy'].append(np.mean(kf_results['training_accuracy']))
-				results['testing_log_loss'].append(np.mean(kf_results['testing_log_loss']))
-				results['testing_accuracy'].append(np.mean(kf_results['testing_accuracy']))
-				results['layer_count'].append(i+1)
-				results['neuron_counts'].append(layer_neurons)
-				results['dropout_rates'].append(drs)
-				results['k_cross_folds'].append(k_cross_folds)
-				results['example_count'].append(example_count)
-				results['iteration_count'].append(iteration_count)
-				results['batch_size'].append(batch_size)
+		n += 1
 
 	# Output results
 	print
