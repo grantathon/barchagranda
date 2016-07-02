c267a8d8b9cb3dc2bbbbfe12bed55f37a0a44f86
grantathon <grant.bartel@gmail.com>
2016-07-02 23:04:46 +0200

    Added bash shell script to initialize barchagranda environment

---
+++ init_barchagranda_env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+echo "Initializing barchagranda environment..."
+echo
+
+# Install python packages
+sudo pip2 install dropbox scikit-learn pandas
+
+echo
+echo "Barchagranda environment initialized!"
